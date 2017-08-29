aa0c1d560be266407e717e879da2e2333e6cae05
kev7862 <kevmatite7862@gmail.com>
2017-08-29 14:22:41 +0300

    We're running a test to return all database entries

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -38,4 +38,14 @@ public void equals_returnsTrueIfNameAndEmailAreSame_true() {
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
+  @Test
+ public void all_returnsAllInstancesOfPerson_true() {
+   Person firstPerson = new Person("Henry", "henry@henry.com");
+   firstPerson.save();
+   Person secondPerson = new Person("Harriet", "harriet@harriet.com");
+   secondPerson.save();
+   assertEquals(true, Person.all().get(0).equals(firstPerson));
+   assertEquals(true, Person.all().get(1).equals(secondPerson));
+ }
+
 }
